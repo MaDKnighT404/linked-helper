d23f9a0e5a1ae522091ff4bd3aa68c60ea00b6a8
George Koloidi <Battleaxe@bk.ru>
2023-07-11 16:16:00 +0200

    feat: add base structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__wrapper}>
+        <img src="../assets/logo.svg" className={styles.header__logo} alt="logo" />
+        <h1 className={styles.header__title}>Message Template Editor</h1>
+        <div></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import styles from './Main.module.scss';
+
+const Main = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.main__wrapper}></div>
+    </main>
+  );
+};
+
+export default Main;
